f66ea4bf19131f9e4f8620819ff9bdc2e10c0833
Alessandro <abonomo@hsr.ch>
2017-10-26 19:22:47 +0200

    TabNavigation, Loans Fragment und Reservations Fragment erstellt

--- app/src/main/java/com/example/sinth/mgeminipr/GadgetsActivity.java
+++ app/src/main/java/com/example/sinth/mgeminipr/GadgetsActivity.java
@@ -1,15 +1,9 @@
 package com.example.sinth.mgeminipr;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.ListView;
-
-import java.util.List;
-
-import domain.Gadget;
-import service.Callback;
-import service.LibraryService;
 
 public class GadgetsActivity extends AppCompatActivity {
 
@@ -18,37 +12,30 @@ public class GadgetsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gadgets);
 
-       // LibraryService.setServerAddress("http://mge1.dev.ifs.hsr.ch/public​");
-        LibraryService.getGadgets(new Callback<List<Gadget>>() {
-            @Override
-            public void onCompletion(List<Gadget> input) {
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new LoanFragment(), "Loans");
+        adapter.addFragment(new ReservationFragment(), "Reservations");
+        viewPager.setAdapter(adapter);
 
-                ListView view = (ListView) findViewById(R.id.gadgets);
-
-                MyArrayAdapter<Gadget> arrayAdapter = new MyArrayAdapter<> (GadgetsActivity.this, R.layout.layout_row, R.id.label, input);
-
-
-                view.setAdapter(arrayAdapter);
-
-            }
-
-            @Override
-            public void onError(String message) {
-                Log.d(message, message);
-            }
-        });
-        /*LibraryService.getGadgets(new Callback<List<Gadget>>() {
-            @Override
-            public void onCompletion(List<Gadget> input) {
-                ListView view = (ListView) findViewById(R.id.listView);
-                ArrayAdapter<Gadget> arrayAdapter = new ArrayAdapter<> (view.getContext(), android.R.layout.simple_list_item_1, input);
-                view.setAdapter(arrayAdapter);
-            }
-
-            @Override
-            public void onError(String message) {
-
-            }
-        });*/
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
+
+
+
+    // LibraryService.setServerAddress("http://mge1.dev.ifs.hsr.ch/public​");
+    /*LibraryService.getGadgets(new Callback<List<Gadget>>() {
+        @Override
+        public void onCompletion(List<Gadget> input) {
+            ListView view = (ListView) findViewById(R.id.gadgets);
+            MyArrayAdapter<Gadget> arrayAdapter = new MyArrayAdapter<> (GadgetsActivity.this, R.layout.layout_row, R.id.label, input);
+            view.setAdapter(arrayAdapter);
+        }
+
+        @Override
+        public void onError(String message) {
+            Log.d(message, message);
+        }
+    });*/
---
+++ app/src/main/java/com/example/sinth/mgeminipr/LoanFragment.java
@@ -0,0 +1,64 @@
+package com.example.sinth.mgeminipr;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import domain.Loan;
+import service.Callback;
+import service.LibraryService;
+
+public class LoanFragment extends Fragment {
+    private RecyclerView recyclerView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_loan, container, false);
+
+        LibraryService.getLoansForCustomer(new Callback<List<Loan>>() {
+            @Override
+            public void onCompletion(List<Loan> input) {
+                List<String> items = new ArrayList<>();
+                for(int i = 0; i < input.size(); i++) {
+                    items.add(input.get(i).getGadget().getName());
+                }
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(items);
+                recyclerView.setAdapter(recyclerAdapter);
+            }
+
+            @Override
+            public void onError(String message) {
+                Log.d(message, message);
+            }
+        });
+
+        /*LibraryService.getGadgets(new Callback<List<Gadget>>() {
+            @Override
+            public void onCompletion(List<Gadget> input) {
+                List<String> items = new ArrayList<>();
+                for(int i = 0; i < input.size(); i++) {
+                    items.add(input.get(i).getName());
+                }
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                RecyclerAdapter recyclerAdapter = new RecyclerAdapter(items);
+                recyclerView.setAdapter(recyclerAdapter);
+            }
+
+            @Override
+            public void onError(String message) {
+
+            }
+        });*/
+
+        return recyclerView;
+    }
+}
