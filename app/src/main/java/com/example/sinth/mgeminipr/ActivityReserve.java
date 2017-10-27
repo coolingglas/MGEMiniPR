32fbe142497dad976aed26b674f17e7f78efcb91
Sinthujan <sinthujan_1991@hotmail.com>
2017-10-27 13:26:39 +0200

    paar änderiga

---
+++ app/src/main/java/com/example/sinth/mgeminipr/ActivityReserve.java
@@ -0,0 +1,16 @@
+package com.example.sinth.mgeminipr;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityReserve extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reserve);
+
+      //  LibraryService.reserveGadget(Ga);
+
+    }
+}
--- app/src/main/java/com/example/sinth/mgeminipr/GetGadgets.java
+++ app/src/main/java/com/example/sinth/mgeminipr/GetGadgets.java
@@ -3,11 +3,14 @@ package com.example.sinth.mgeminipr;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.List;
 
 import domain.Gadget;
+import domain.Reservation;
 import service.Callback;
 import service.LibraryService;
 
@@ -23,12 +26,57 @@ public class GetGadgets extends AppCompatActivity {
             @Override
             public void onCompletion(List<Gadget> input) {
 
-                ListView view = (ListView) findViewById(R.id.gadgets);
+                final ListView view = (ListView) findViewById(R.id.gadgets);
 
-                MyArrayAdapter<Gadget> arrayAdapter = new MyArrayAdapter<> (GetGadgets.this, R.layout.layout_row, R.id.label, input);
+                //MyArrayAdapter<Gadget> arrayAdapter = new MyArrayAdapter<> (GetGadgets.this, R.layout.layout_row, R.id.label, input);
+
+               // TextView v = (TextView) findViewById(R.id.);
+
+                //view.setAdapter(arrayAdapter);
 
 
-                view.setAdapter(arrayAdapter);
+
+
+
+//Gadget Reservieren
+                LibraryService.reserveGadget(input.get(0), new Callback<Boolean>() {
+                            @Override
+                            public void onCompletion(Boolean input) {
+                                LibraryService.getReservationsForCustomer(new Callback<List<Reservation>>() {
+                                    @Override
+                                    public void onCompletion(List<Reservation> input) {
+                                        MyArrayAdapterForReservations<Reservation>  adapterForReservations = new MyArrayAdapterForReservations<Reservation>(GetGadgets.this, R.layout.layout_row, R.id.label, input);
+
+                                        view.setAdapter(adapterForReservations);
+
+                                        view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                                                Reservation reservation = (Reservation) parent.getItemAtPosition(position);
+                                                //reservation.setSelected(!module.isSelected());
+                                               // dataAdapter.notifyDataSetChanged();
+
+                                               // LibraryService.deleteReservation(new Callback<boolean>());
+                                            }
+                                        });
+
+
+                                    }
+
+                                    @Override
+                                    public void onError(String message) {
+
+                                    }
+                                });
+                            }
+
+                            @Override
+                            public void onError(String message) {
+
+                            }
+                        }
+                );
+
+                view.findViewById(R.id.gadgets);
 
             }
 
